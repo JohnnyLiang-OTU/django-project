43c9214ce24b780e017d313548a4e112875a174a
JohnnyLiang-OTU <johnny.liang@ontariotechu.net>
2024-06-27 18:20:51 -0500

    Finished functionality for brands, and added uniformity to cards in catalog

--- static/js/brands.js
+++ static/js/brands.js
@@ -2,5 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const brandContainer = document.querySelector('.brand_container');
     const firstCard = document.querySelector('.brand_card');
     const buttonWidth = document.querySelector('.prev_button').offsetWidth;
-    brandContainer.scrollLeft = firstCard.offsetWidth - buttonWidth;
+    brandContainer.scrollLeft = (1.2*firstCard.offsetWidth) - buttonWidth;
+
+    const nextBtn = document.querySelector('.next_button');
+    const prevBtn = document.querySelector('.prev_button');
+
+    let containerDimension = brandContainer.getBoundingClientRect();
+    let containerWidth = containerDimension.width;
+        
+    nextBtn.addEventListener('click', () => {
+        if (brandContainer.scrollLeft + containerWidth >= brandContainer.scrollWidth) {
+            brandContainer.scrollLeft = 0;
+        } else {
+            brandContainer.scrollLeft += containerWidth;
+        }
+    });
+
+    prevBtn.addEventListener('click', () => {
+        if (brandContainer.scrollLeft == 0) {
+            brandContainer.scrollLeft = brandContainer.scrollWidth;
+        } else {
+            brandContainer.scrollLeft -= containerWidth;
+        }
+    });
 });
