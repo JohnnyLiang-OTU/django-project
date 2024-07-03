3b0021379ece50905540c782a22008c710991eb8
JohnnyLiang-OTU <johnny.liang@ontariotechu.net>
2024-07-03 15:36:38 -0500

    small retouchings

--- static/js/brands.js
+++ static/js/brands.js
@@ -1,28 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const brandContainer = document.querySelector('.brand_container');
-    const firstCard = document.querySelector('.brand_card');
-    const buttonWidth = document.querySelector('.prev_button').offsetWidth;
-    brandContainer.scrollLeft = (1.2*firstCard.offsetWidth) - buttonWidth;
+// document.addEventListener('DOMContentLoaded', function() {
+    // const brandContainer = document.querySelector('.brand_container');
+    // const firstCard = document.querySelector('.brand_card');
+    // const buttonWidth = document.querySelector('.prev_button').offsetWidth;
+    // brandContainer.scrollLeft = (firstCard.offsetWidth) - buttonWidth;
 
-    const nextBtn = document.querySelector('.next_button');
-    const prevBtn = document.querySelector('.prev_button');
+    // const nextBtn = document.querySelector('.next_button');
+    // const prevBtn = document.querySelector('.prev_button');
 
-    let containerDimension = brandContainer.getBoundingClientRect();
-    let containerWidth = containerDimension.width;
+    // let containerDimension = brandContainer.getBoundingClientRect();
+    // let containerWidth = containerDimension.width;
         
-    nextBtn.addEventListener('click', () => {
-        if (brandContainer.scrollLeft + containerWidth >= brandContainer.scrollWidth) {
-            brandContainer.scrollLeft = 0;
-        } else {
-            brandContainer.scrollLeft += containerWidth;
-        }
-    });
+    // nextBtn.addEventListener('click', () => {
+    //     if (brandContainer.scrollLeft + containerWidth >= brandContainer.scrollWidth) {
+    //         brandContainer.scrollLeft = 0;
+    //     } else {
+    //         brandContainer.scrollLeft += containerWidth;
+    //     }
+    // });
 
-    prevBtn.addEventListener('click', () => {
-        if (brandContainer.scrollLeft == 0) {
-            brandContainer.scrollLeft = brandContainer.scrollWidth;
-        } else {
-            brandContainer.scrollLeft -= containerWidth;
-        }
-    });
-});
+    // prevBtn.addEventListener('click', () => {
+    //     if (brandContainer.scrollLeft == 0) {
+    //         brandContainer.scrollLeft = brandContainer.scrollWidth;
+    //     } else {
+    //         brandContainer.scrollLeft -= containerWidth;
+    //     }
+    // });
+// });
